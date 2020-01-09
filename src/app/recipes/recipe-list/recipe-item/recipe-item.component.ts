2f6ab7bc9a0fbb4c6e848304a29a176486c850c7
mateigherceanu <matei-dumitru.gherceanu@metrobank.plm.uk>
2020-01-09 14:23:13 +0200

    Course Project - Components & Databinding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RecipeModel } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: RecipeModel;
+  @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSelectRecipe() {
+    this.recipeSelected.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { RecipeModel } from '../recipe.model';
 
 @Component({
@@ -9,8 +9,9 @@ import { RecipeModel } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: RecipeModel[] = [
     new RecipeModel('A test recipe', 'best recipe ever', 'https://cdn.apartmenttherapy.info/image/upload/v1567541461/k/Photo/Recipes/2019-09-how-to-shrimp-alfredo/HT-Shrimp-Alfredo_103.jpg'),
-    new RecipeModel('A test recipe', 'best recipe ever', 'https://cdn.apartmenttherapy.info/image/upload/v1567541461/k/Photo/Recipes/2019-09-how-to-shrimp-alfredo/HT-Shrimp-Alfredo_103.jpg')
+    new RecipeModel('Another test recipe', 'Almost best recipe ever', 'https://cdn.apartmenttherapy.info/image/upload/v1567541461/k/Photo/Recipes/2019-09-how-to-shrimp-alfredo/HT-Shrimp-Alfredo_103.jpg')
   ];
+  @Output() recipeWasSelected = new EventEmitter<RecipeModel>();
 
   constructor() {
   }
@@ -18,4 +19,8 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: RecipeModel) {
+    console.log(recipe)
+    this.recipeWasSelected.emit(recipe);
+  }
 }
