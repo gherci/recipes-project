cec41eda595b9943f9576c44b5857a08ffa1b607
mateigherceanu <matei-dumitru.gherceanu@metrobank.plm.uk>
2020-02-10 17:01:26 +0200

    Course Project - Services and dependency injection

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { RecipeModel } from './recipe.model';
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Injectable } from '@angular/core';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<RecipeModel>();
+  private recipes: RecipeModel[] = [
+    new RecipeModel(
+      'A tasty schnitzel',
+      'Super tasty schnitzel- yum yum!',
+      'https://natashaskitchen.com/wp-content/uploads/2016/02/Pork-Schnitzel-Recipe-7-600x900.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new RecipeModel(
+      'Tonny\'s shrimp pasta',
+      'Almost best recipe ever',
+      'https://cdn.apartmenttherapy.info/image/upload/v1567541461/k/Photo/Recipes/2019-09-how-to-shrimp-alfredo/HT-Shrimp-Alfredo_103.jpg',
+      [
+        new Ingredient('Pasta', 20),
+        new Ingredient('Shrimp', 8),
+        new Ingredient('Tomato', 2),
+        new Ingredient('Parmesan', 15)
+      ])
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  getRecipes(): RecipeModel[] {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,24 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
